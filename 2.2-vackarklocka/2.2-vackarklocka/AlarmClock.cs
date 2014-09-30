e7936bae7e524c7ff68345bdd5f9bebeb1804416
al222jf <al222jf@student.lnu.se>
2014-09-30 13:01:52 +0200

    Started working with the class

---
+++ 2.2-vackarklocka/2.2-vackarklocka/AlarmClock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2_vackarklocka
+{
+    class AlarmClock
+    {
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
+
+        public int AlarmHour
+        {
+            get { return _alarmHour; }
+
+            set
+            {
+                if (value > 23 || value < 0)
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+    }
+}
